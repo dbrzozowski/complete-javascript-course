f66dc238688e08a6ce2f742110ef2d21addfee67
Damian Brzozowski <damian@brzozowski.pro>
2022-05-02 11:13:18 +0200

    fundamentals1: 11 Pactice Assignments

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,6 @@
+let country = 'Poland';
+let continent = 'Europe';
+let population = 37.95;
+console.log('🚀 ~ file: assignments.js ~ line 2 ~ country', country);
+console.log('🚀 ~ file: assignments.js ~ line 3 ~ continent', continent);
+console.log('🚀 ~ file: assignments.js ~ line 5 ~ population', population);
