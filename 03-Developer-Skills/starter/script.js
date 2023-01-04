999bd8e999cf53fae73b1820f8cc882c88319475
Damian Brzozowski <damian@brzozowski.pro>
2023-01-04 13:32:29 +0100

    developerSkills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+function printForecast(arr) {
+  let str = '';
+  for (let i = 0; i <= arr.length - 1; i++) {
+    str += `... ${arr[i]}ºC in ${i + 1} days `;
+  }
+  return str;
+}
+
+console.log(printForecast([17, 21, 23]));
+console.log(printForecast([12, 5, -5, 0, 4]));
